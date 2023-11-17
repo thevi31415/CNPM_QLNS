656c2e64097d748df8824c2282b50b0dbab38010
Bao An Thai <baoanthai190503@gmail.com>
2023-11-17 10:34:27 +0700

    Them BL_DuAn

---
+++ CNPM_QLNS/BS_Layer/BL_DuAn.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNPM_QLNS.BS_Layer
+{
+	internal class BL_DuAn
+	{
+	}
+}
