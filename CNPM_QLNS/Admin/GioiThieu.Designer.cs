5b74427370d96c864e7d2c4e636a5875e59251ef
TruongVanSinh <21110919@student.hcmute.edu.vn>
2023-11-22 14:39:21 +0700

    hoan thanh chuc nang luong

--- CNPM_QLNS/Admin/GioiThieu.Designer.cs
+++ CNPM_QLNS/Admin/GioiThieu.Designer.cs
@@ -35,7 +35,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -43,9 +43,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.DeepSkyBlue;
-            this.label1.Location = new System.Drawing.Point(155, 78);
+            this.label1.Location = new System.Drawing.Point(138, 62);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(826, 59);
+            this.label1.Size = new System.Drawing.Size(681, 51);
             this.label1.TabIndex = 0;
             this.label1.Text = "ỨNG DỤNG QUẢN LÝ NHÂN SỰ";
             // 
@@ -53,27 +53,27 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(397, 199);
+            this.label4.Location = new System.Drawing.Point(353, 159);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(312, 40);
+            this.label4.Size = new System.Drawing.Size(263, 36);
             this.label4.TabIndex = 3;
             this.label4.Text = "Nguyễn Đại Phúc";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(498, 464);
+            this.label6.Location = new System.Drawing.Point(443, 371);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(0, 20);
+            this.label6.Size = new System.Drawing.Size(0, 16);
             this.label6.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(397, 271);
+            this.label2.Location = new System.Drawing.Point(353, 217);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(390, 40);
+            this.label2.Size = new System.Drawing.Size(330, 36);
             this.label2.TabIndex = 7;
             this.label2.Text = "Nguyễn Dương Thế Vĩ";
             // 
@@ -81,9 +81,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(397, 339);
+            this.label3.Location = new System.Drawing.Point(353, 271);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(262, 40);
+            this.label3.Size = new System.Drawing.Size(222, 36);
             this.label3.TabIndex = 8;
             this.label3.Text = "Võ Trọng Hiếu";
             // 
@@ -91,9 +91,9 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(197, 402);
+            this.label8.Location = new System.Drawing.Point(175, 322);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(590, 40);
+            this.label8.Size = new System.Drawing.Size(498, 36);
             this.label8.TabIndex = 9;
             this.label8.Text = "21110945 - Hán Quốc Chế Vương";
             // 
@@ -101,29 +101,29 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(403, 464);
+            this.label9.Location = new System.Drawing.Point(358, 371);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(226, 40);
+            this.label9.Size = new System.Drawing.Size(191, 36);
             this.label9.TabIndex = 10;
             this.label9.Text = "Thái Bảo An";
             // 
-            // label5
+            // label7
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(404, 539);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(302, 40);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "Trương Văn Sinh";
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(175, 438);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(412, 36);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "2110919 - Trương Văn Sinh";
             // 
             // GioiThieu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1066, 722);
-            this.Controls.Add(this.label5);
+            this.ClientSize = new System.Drawing.Size(948, 578);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label3);
@@ -132,7 +132,6 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "GioiThieu";
             this.Text = "GioiThieu";
             this.Load += new System.EventHandler(this.GioiThieu_Load);
@@ -150,6 +149,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
     }
 }
